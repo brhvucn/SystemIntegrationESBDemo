05b2ae7278c98f23de6a9dec0f7c7337faf06a99
Brian Hvarregaard <brian@median.dk>
2022-10-24 13:40:35 +0200

    First Commit of Hello World

---
+++ Demo1.Producer/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+//code reference: https://www.rabbitmq.com/tutorials/tutorial-one-dotnet.html
+Console.WriteLine("Demo 1 - Hello World from RabbitMQ");
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "hello",
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+    string message = "Hello World!";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "",
+                         routingKey: "hello",
+                         basicProperties: null,
+                         body: body);
+    Console.WriteLine(" [x] Sent {0}", message);
+}
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
